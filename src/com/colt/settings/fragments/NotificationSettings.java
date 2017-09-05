d5a14d1399bc6ba72882e4f34aa540943c69bbb9
beanstown106 <nbenis106@gmail.com>
2017-09-05 14:44:44 +0800

    incall vibration options [3/3] *allow setting vibration when call is connected *allow setting vibration when call is disconnected *allow setting vibration for call waiting *this works with google and aosp dialer :)

---
+++ src/com/colt/settings/fragments/NotificationSettings.java
@@ -0,0 +1,33 @@
+package com.colt.settings.fragments;
+import com.android.internal.logging.nano.MetricsProto;
+import android.os.Bundle;
+import com.android.settings.R;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.colt.settings.utils.Utils;
+
+public class NotificationSettings extends SettingsPreferenceFragment {
+
+    private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.colt_settings_notifications);
+        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
+        if (!Utils.isVoiceCapable(getActivity())) {
+            prefScreen.removePreference(incallVibCategory);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.COLT;
+    }
+}
