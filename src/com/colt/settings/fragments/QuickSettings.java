60ca003cf69dca455ee64b21ffa4f2e191a1c903
RakeshBatra <raakesh.batra@rediffmail.com>
2019-09-30 16:29:57 +0000

    ColtCenter: Add QS panel opacity

--- src/com/colt/settings/fragments/QuickSettings.java
+++ src/com/colt/settings/fragments/QuickSettings.java
@@ -24,9 +24,14 @@ import android.view.View;
 import java.util.List;
 import java.util.ArrayList;
 
+import com.colt.settings.preference.CustomSeekBarPreference;
+
 public class QuickSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private static final String KEY_QS_PANEL_ALPHA = "qs_panel_alpha";
+
+    private CustomSeekBarPreference mQsPanelAlpha;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -37,11 +42,25 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         PreferenceScreen prefScreen = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
 
+	mQsPanelAlpha = (CustomSeekBarPreference) findPreference(KEY_QS_PANEL_ALPHA);
+        int qsPanelAlpha = Settings.System.getInt(getContentResolver(),
+                Settings.System.OMNI_QS_PANEL_BG_ALPHA, 221);
+        mQsPanelAlpha.setValue((int)(((double) qsPanelAlpha / 255) * 100));
+        mQsPanelAlpha.setOnPreferenceChangeListener(this);
+
+
         }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-
+	ContentResolver resolver = getActivity().getContentResolver();
+	if (preference == mQsPanelAlpha) {
+            int bgAlpha = (Integer) newValue;
+            int trueValue = (int) (((double) bgAlpha / 100) * 255);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.OMNI_QS_PANEL_BG_ALPHA, trueValue);
+            return true;
+	}
         return false;
     }
 
