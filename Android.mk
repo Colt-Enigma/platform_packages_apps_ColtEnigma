1446aa3405ad18fd8230bec12d318f0b293e64de
RakeshBatra <raakesh.batra@rediffmail.com>
2019-10-14 01:03:20 +0000

    About team AndroidX adoption
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- Android.mk
+++ Android.mk
@@ -1,9 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
 LOCAL_STATIC_ANDROID_LIBRARIES += \
-     android-support-constraint-layout \
-     android-support-v4 \
-     android-support-v7-appcompat \
-     android-support-v7-cardview \
+     androidx-constraintlayout_constraintlayout \
+     androidx.appcompat_appcompat \
+     androidx.cardview_cardview \
+     androidx.recyclerview_recyclerview \
 
  LOCAL_STATIC_JAVA_LIBRARIES += glide
