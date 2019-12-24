02b158ad33f593711ce0cba298adffcd231a3937
RakeshBatra <raakesh.batra@rediffmail.com>
2019-12-24 16:37:21 +0000

    Notch friendly network traffic [2/2]

--- src/com/colt/settings/fragments/StatusBarSettings.java
+++ src/com/colt/settings/fragments/StatusBarSettings.java
@@ -29,9 +29,11 @@ import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
 import android.provider.Settings;
+import com.android.internal.util.colt.ColtUtils;
 
 import com.colt.settings.preference.CustomSeekBarPreference;
 import com.colt.settings.preference.SystemSettingSwitchPreference;
+import com.colt.settings.preference.SystemSettingSeekBarPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.SettingsPreferenceFragment;
@@ -43,9 +45,13 @@ import java.util.List;
 
 public class StatusBarSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
-    private CustomSeekBarPreference mThreshold;
-    private SystemSettingSwitchPreference mNetMonitor;
+    private static final String KEY_NETWORK_TRAFFIC = "network_traffic_location";
+    private static final String KEY_NETWORK_TRAFFIC_ARROW = "network_traffic_arrow";
+    private static final String KEY_NETWORK_TRAFFIC_AUTOHIDE = "network_traffic_autohide_threshold";
 
+    private ListPreference mNetworkTraffic;
+    private SystemSettingSwitchPreference mNetworkTrafficArrow;
+    private SystemSettingSeekBarPreference mNetworkTrafficAutohide;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -55,41 +61,55 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements OnP
         PreferenceScreen prefSet = getPreferenceScreen();
 	final ContentResolver resolver = getActivity().getContentResolver();
 
-        boolean isNetMonitorEnabled = Settings.System.getIntForUser(resolver,
-                Settings.System.NETWORK_TRAFFIC_STATE, 1, UserHandle.USER_CURRENT) == 1;
-        mNetMonitor = (SystemSettingSwitchPreference) findPreference("network_traffic_state");
-        mNetMonitor.setChecked(isNetMonitorEnabled);
-        mNetMonitor.setOnPreferenceChangeListener(this);
+	mNetworkTraffic = (ListPreference) findPreference(KEY_NETWORK_TRAFFIC);
+        int networkTraffic = Settings.System.getInt(resolver,
+        Settings.System.NETWORK_TRAFFIC_LOCATION, 0);
+        CharSequence[] NonNotchEntries = { getResources().getString(R.string.network_traffic_disabled),
+                getResources().getString(R.string.network_traffic_statusbar),
+                getResources().getString(R.string.network_traffic_qs_header) };
+        CharSequence[] NotchEntries = { getResources().getString(R.string.network_traffic_disabled),
+		getResources().getString(R.string.network_traffic_statusbar),
+                getResources().getString(R.string.network_traffic_qs_header) };
+        CharSequence[] NonNotchValues = {"0", "1" , "2"};
+	CharSequence[] NotchValues = {"0", "1" , "2"};
+        mNetworkTraffic.setEntries(ColtUtils.hasNotch(getActivity()) ? NotchEntries : NonNotchEntries);
+        mNetworkTraffic.setEntryValues(ColtUtils.hasNotch(getActivity()) ? NotchValues : NonNotchValues);
+        mNetworkTraffic.setValue(String.valueOf(networkTraffic));
+        mNetworkTraffic.setSummary(mNetworkTraffic.getEntry());
+        mNetworkTraffic.setOnPreferenceChangeListener(this);
 
-        int value = Settings.System.getIntForUser(resolver,
-                Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, 1, UserHandle.USER_CURRENT);
-        mThreshold = (CustomSeekBarPreference) findPreference("network_traffic_autohide_threshold");
-        mThreshold.setValue(value);
-        mThreshold.setOnPreferenceChangeListener(this);
-        mThreshold.setEnabled(isNetMonitorEnabled);
+        mNetworkTrafficArrow = (SystemSettingSwitchPreference) findPreference(KEY_NETWORK_TRAFFIC_ARROW);
+	mNetworkTrafficAutohide = (SystemSettingSeekBarPreference) findPreference(KEY_NETWORK_TRAFFIC_AUTOHIDE);
+        updateNetworkTrafficPrefs(networkTraffic);
 
    }
 
  @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-	if (preference == mNetMonitor) {
-            boolean value = (Boolean) objValue;
-            Settings.System.putIntForUser(getActivity().getContentResolver(),
-                    Settings.System.NETWORK_TRAFFIC_STATE, value ? 1 : 0,
-                    UserHandle.USER_CURRENT);
-            mNetMonitor.setChecked(value);
-            mThreshold.setEnabled(value);
-            return true;
-        } else if (preference == mThreshold) {
-            int val = (Integer) objValue;
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, val,
-                    UserHandle.USER_CURRENT);
+	AlertDialog dialog;
+	if (preference == mNetworkTraffic) {
+            int networkTraffic = Integer.valueOf((String) objValue);
+            int index = mNetworkTraffic.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NETWORK_TRAFFIC_LOCATION, networkTraffic);
+            mNetworkTraffic.setSummary(mNetworkTraffic.getEntries()[index]);
+            updateNetworkTrafficPrefs(networkTraffic);
             return true;
         }
         return false;
     }
 
+    private void updateNetworkTrafficPrefs(int networkTraffic) {
+        if (mNetworkTraffic != null) {
+            if (networkTraffic == 0) {
+                mNetworkTrafficArrow.setEnabled(false);
+                mNetworkTrafficAutohide.setEnabled(false);
+            } else {
+                mNetworkTrafficArrow.setEnabled(true);
+                mNetworkTrafficAutohide.setEnabled(true);
+            }
+        }
+    }
 
 @Override
     public int getMetricsCategory() {
