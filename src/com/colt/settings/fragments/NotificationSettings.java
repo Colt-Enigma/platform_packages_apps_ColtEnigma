ef7bd737755672581128e9a47844807c3f26e208
RakeshBatra <raakesh.batra@rediffmail.com>
2019-12-28 17:20:02 +0000

    Give Ambient Light a Separate Space
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- src/com/colt/settings/fragments/NotificationSettings.java
+++ src/com/colt/settings/fragments/NotificationSettings.java
@@ -1,29 +1,22 @@
 package com.colt.settings.fragments;
 import com.android.internal.logging.nano.MetricsProto;
 import android.os.Bundle;
-import android.os.UserHandle;
 import com.android.settings.R;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 
 import com.android.settings.SettingsPreferenceFragment;
-import net.margaritov.preference.colorpicker.ColorPickerPreference;
 import com.colt.settings.preference.AmbientLightSettingsPreview;
 import android.provider.Settings;
 import com.colt.settings.preference.CustomSeekBarPreference;
 
 import com.colt.settings.utils.Utils;
 
-public class NotificationSettings extends SettingsPreferenceFragment
-        implements Preference.OnPreferenceChangeListener {
+public class NotificationSettings extends SettingsPreferenceFragment {
 
     private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
-    private static final String PULSE_AMBIENT_LIGHT_COLOR = "pulse_ambient_light_color";
-    private static final String PULSE_AMBIENT_LIGHT_DURATION = "pulse_ambient_light_duration";
 
-    private ColorPickerPreference mEdgeLightColorPreference;
-    private CustomSeekBarPreference mEdgeLightDurationPreference;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -35,53 +28,6 @@ public class NotificationSettings extends SettingsPreferenceFragment
         if (!Utils.isVoiceCapable(getActivity())) {
             prefScreen.removePreference(incallVibCategory);
         }
-
-	mEdgeLightColorPreference = (ColorPickerPreference) findPreference(PULSE_AMBIENT_LIGHT_COLOR);
-        int edgeLightColor = Settings.System.getInt(getContentResolver(),
-                Settings.System.PULSE_AMBIENT_LIGHT_COLOR, 0xFF3980FF);
-	AmbientLightSettingsPreview.setAmbientLightPreviewColor(edgeLightColor);
-        mEdgeLightColorPreference.setNewPreviewColor(edgeLightColor);
-	String edgeLightColorHex = ColorPickerPreference.convertToRGB(edgeLightColor);
-        if (edgeLightColorHex.equals("#3980ff")) {
-            mEdgeLightColorPreference.setSummary(R.string.default_string);
-        } else {
-            mEdgeLightColorPreference.setSummary(edgeLightColorHex);
-        }
-        mEdgeLightColorPreference.setOnPreferenceChangeListener(this);
-
-	mEdgeLightDurationPreference = (CustomSeekBarPreference) findPreference(PULSE_AMBIENT_LIGHT_DURATION);
-        int lightDuration = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.PULSE_AMBIENT_LIGHT_DURATION, 2, UserHandle.USER_CURRENT);
-        mEdgeLightDurationPreference.setValue(lightDuration);
-        mEdgeLightDurationPreference.setOnPreferenceChangeListener(this);
-    }
-
-     @Override
-    public void onResume() {
-        super.onResume();
-    }
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-	if (preference == mEdgeLightColorPreference) {
-	   String hex = ColorPickerPreference.convertToRGB(
-                    Integer.valueOf(String.valueOf(newValue)));
-            if (hex.equals("#3980ff")) {
-                preference.setSummary(R.string.default_string);
-            } else {
-                preference.setSummary(hex);
-            }
-	    AmbientLightSettingsPreview.setAmbientLightPreviewColor(Integer.valueOf(String.valueOf(newValue)));
-            int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.PULSE_AMBIENT_LIGHT_COLOR, intHex);
-            return true;
-	} else if (preference == mEdgeLightDurationPreference) {
-            int value = (Integer) newValue;
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.PULSE_AMBIENT_LIGHT_DURATION, value, UserHandle.USER_CURRENT);
-            return true;
-        }
-        return false;
     }
 
     @Override
