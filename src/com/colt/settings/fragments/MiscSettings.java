fryevia/platform_packages_apps_DerpQuest

DerpQuest: Make aspect ratio hack configable on app level [2/2]
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

src/com/derpquest/settings/fragments/MiscSettings.java
@@ -18,21 +18,91 @@ package com.derpquest.settings.fragments;
 import com.android.internal.logging.nano.MetricsProto;
 
 import android.os.Bundle;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.SwitchPreference;
+import android.provider.Settings;
+import android.text.TextUtils;
 import com.android.settings.R;
+import com.derpquest.settings.preferences.AppMultiSelectListPreference;
+import com.derpquest.settings.preferences.ScrollAppsViewPreference;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 
 import com.android.settings.SettingsPreferenceFragment;
 
-public class MiscSettings extends SettingsPreferenceFragment {
+public class MiscSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String KEY_ASPECT_RATIO_APPS_ENABLED = "aspect_ratio_apps_enabled";
+    private static final String KEY_ASPECT_RATIO_APPS_LIST = "aspect_ratio_apps_list";
+    private static final String KEY_ASPECT_RATIO_CATEGORY = "aspect_ratio_category";
+    private static final String KEY_ASPECT_RATIO_APPS_LIST_SCROLLER = "aspect_ratio_apps_list_scroller";
+
+    private AppMultiSelectListPreference mAspectRatioAppsSelect;
+    private ScrollAppsViewPreference mAspectRatioApps;
+
 
     @Override
     public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+      super.onCreate(icicle);
 
-        addPreferencesFromResource(R.xml.derpquest_settings_misc);
+      addPreferencesFromResource(R.xml.derpquest_settings_misc);
+
+      final PreferenceCategory aspectRatioCategory =
+          (PreferenceCategory) getPreferenceScreen().findPreference(KEY_ASPECT_RATIO_CATEGORY);
+      final boolean supportMaxAspectRatio =
+          getResources().getBoolean(com.android.internal.R.bool.config_haveHigherAspectRatioScreen);
+      if (!supportMaxAspectRatio) {
+          getPreferenceScreen().removePreference(aspectRatioCategory);
+      } else {
+        mAspectRatioAppsSelect =
+            (AppMultiSelectListPreference) findPreference(KEY_ASPECT_RATIO_APPS_LIST);
+        mAspectRatioApps =
+            (ScrollAppsViewPreference) findPreference(KEY_ASPECT_RATIO_APPS_LIST_SCROLLER);
+        final String valuesString = Settings.System.getString(getContentResolver(),
+            Settings.System.OMNI_ASPECT_RATIO_APPS_LIST);
+        List<String> valuesList = new ArrayList<String>();
+        if (!TextUtils.isEmpty(valuesString)) {
+          valuesList.addAll(Arrays.asList(valuesString.split(":")));
+          mAspectRatioApps.setVisible(true);
+          mAspectRatioApps.setValues(valuesList);
+        } else {
+          mAspectRatioApps.setVisible(false);
+        }
+        mAspectRatioAppsSelect.setValues(valuesList);
+        mAspectRatioAppsSelect.setOnPreferenceChangeListener(this);
+      }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+      if (preference == mAspectRatioAppsSelect) {
+        Collection<String> valueList = (Collection<String>) objValue;
+        mAspectRatioApps.setVisible(false);
+        if (valueList != null) {
+          Settings.System.putString(getContentResolver(),
+              Settings.System.OMNI_ASPECT_RATIO_APPS_LIST, TextUtils.join(":", valueList));
+          mAspectRatioApps.setVisible(true);
+          mAspectRatioApps.setValues(valueList);
+        } else {
+          Settings.System.putString(getContentResolver(),
+              Settings.System.OMNI_ASPECT_RATIO_APPS_LIST, "");
+        }
+        return true;
+      }
+      return false;
     }
 
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.OWLSNEST;
+      return MetricsProto.MetricsEvent.OWLSNEST;
     }
 }

src/com/derpquest/settings/preferences/ScrollAppsViewPreference.java
@@ -1,0 +1,93 @@
+/*
+ * Copyright (C) 2017 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.derpquest.settings.preferences;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.android.settings.R;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class ScrollAppsViewPreference extends Preference {
+    private static final String TAG = "ScrollAppsPreference";
+
+    private Context mContext;
+    private List<String> mValues = new ArrayList<String>();
+    private PackageManager mPm;
+    private LayoutInflater mInflater;
+
+    public ScrollAppsViewPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        initPreference(context);
+    }
+
+    public ScrollAppsViewPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initPreference(context);
+    }
+
+    public ScrollAppsViewPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initPreference(context);
+    }
+
+    public void setValues(Collection<String> values) {
+        mValues.clear();
+        mValues.addAll(values);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        LinearLayout linearLayout = (LinearLayout) holder.findViewById(R.id.selected_apps);
+        if (linearLayout.getChildCount() > 0) linearLayout.removeAllViews();
+
+        for (String value : mValues) {
+            try {
+                View v = mInflater.inflate(R.layout.app_grid_item, null);
+                ComponentName componentName = ComponentName.unflattenFromString(value);
+                Drawable icon = mPm.getActivityIcon(componentName);
+                ((ImageView) v.findViewById(R.id.appIcon)).setImageDrawable(icon);
+                v.setPadding(10, 5, 10, 5);
+                linearLayout.addView(v);
+            } catch (PackageManager.NameNotFoundException e) {
+                Log.e(TAG, "Set app icon", e);
+            }
+        }
+    }
+
+    private void initPreference(Context context) {
+        mContext = context;
+        setLayoutResource(R.layout.preference_selected_apps_view);
+        mPm = context.getPackageManager();
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+}

Colt-Enigma/platform_packages_apps_ColtEnigma

src/com/colt/settings/fragments/MiscSettings.java
@@ -22,14 +22,28 @@ import androidx.preference.SwitchPreference;
 import android.provider.Settings;
 import com.android.settings.R;
 import com.colt.settings.utils.Utils;
+import android.text.TextUtils;
+import com.colt.settings.preference.AppMultiSelectListPreference;
+import com.colt.settings.preference.ScrollAppsViewPreference;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import com.android.settings.SettingsPreferenceFragment;
 
 public class MiscSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
+
+    private static final String KEY_ASPECT_RATIO_APPS_ENABLED = "aspect_ratio_apps_enabled";
+    private static final String KEY_ASPECT_RATIO_APPS_LIST = "aspect_ratio_apps_list";
+    private static final String KEY_ASPECT_RATIO_CATEGORY = "aspect_ratio_category";
+    private static final String KEY_ASPECT_RATIO_APPS_LIST_SCROLLER = "aspect_ratio_apps_list_scroller";
+
+    private AppMultiSelectListPreference mAspectRatioAppsSelect;
+    private ScrollAppsViewPreference mAspectRatioApps;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -39,6 +53,31 @@ public class MiscSettings extends SettingsPreferenceFragment implements
         final String KEY_DEVICE_PART_PACKAGE_NAME = "com.oneplus.shit.settings";
 
         addPreferencesFromResource(R.xml.colt_settings_misc);
+
+      final PreferenceCategory aspectRatioCategory =
+          (PreferenceCategory) getPreferenceScreen().findPreference(KEY_ASPECT_RATIO_CATEGORY);
+      final boolean supportMaxAspectRatio =
+          getResources().getBoolean(com.android.internal.R.bool.config_haveHigherAspectRatioScreen);
+      if (!supportMaxAspectRatio) {
+          getPreferenceScreen().removePreference(aspectRatioCategory);
+      } else {
+        mAspectRatioAppsSelect =
+            (AppMultiSelectListPreference) findPreference(KEY_ASPECT_RATIO_APPS_LIST);
+        mAspectRatioApps =
+            (ScrollAppsViewPreference) findPreference(KEY_ASPECT_RATIO_APPS_LIST_SCROLLER);
+        final String valuesString = Settings.System.getString(getContentResolver(),
+            Settings.System.OMNI_ASPECT_RATIO_APPS_LIST);
+        List<String> valuesList = new ArrayList<String>();
+        if (!TextUtils.isEmpty(valuesString)) {
+          valuesList.addAll(Arrays.asList(valuesString.split(":")));
+          mAspectRatioApps.setVisible(true);
+          mAspectRatioApps.setValues(valuesList);
+        } else {
+          mAspectRatioApps.setVisible(false);
+        }
+        mAspectRatioAppsSelect.setValues(valuesList);
+        mAspectRatioAppsSelect.setOnPreferenceChangeListener(this);
+      }
 
         // DeviceParts
         if (!Utils.isPackageInstalled(getActivity(), KEY_DEVICE_PART_PACKAGE_NAME)) {
@@ -57,7 +96,20 @@ public class MiscSettings extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-
+	if (preference == mAspectRatioAppsSelect) {
+        Collection<String> valueList = (Collection<String>) objValue;
+        mAspectRatioApps.setVisible(false);
+        if (valueList != null) {
+          Settings.System.putString(getContentResolver(),
+              Settings.System.OMNI_ASPECT_RATIO_APPS_LIST, TextUtils.join(":", valueList));
+          mAspectRatioApps.setVisible(true);
+          mAspectRatioApps.setValues(valueList);
+        } else {
+          Settings.System.putString(getContentResolver(),
+              Settings.System.OMNI_ASPECT_RATIO_APPS_LIST, "");
+        }
+        return true;
+      }
         return false;
     }
 

src/com/colt/settings/preference/ScrollAppsViewPreference.java
@@ -1,0 +1,93 @@
+/*
+ * Copyright (C) 2017 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.colt.settings.preference;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.android.settings.R;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class ScrollAppsViewPreference extends Preference {
+    private static final String TAG = "ScrollAppsPreference";
+
+    private Context mContext;
+    private List<String> mValues = new ArrayList<String>();
+    private PackageManager mPm;
+    private LayoutInflater mInflater;
+
+    public ScrollAppsViewPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        initPreference(context);
+    }
+
+    public ScrollAppsViewPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initPreference(context);
+    }
+
+    public ScrollAppsViewPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initPreference(context);
+    }
+
+    public void setValues(Collection<String> values) {
+        mValues.clear();
+        mValues.addAll(values);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        LinearLayout linearLayout = (LinearLayout) holder.findViewById(R.id.selected_apps);
+        if (linearLayout.getChildCount() > 0) linearLayout.removeAllViews();
+
+        for (String value : mValues) {
+            try {
+                View v = mInflater.inflate(R.layout.app_grid_item, null);
+                ComponentName componentName = ComponentName.unflattenFromString(value);
+                Drawable icon = mPm.getActivityIcon(componentName);
+                ((ImageView) v.findViewById(R.id.appIcon)).setImageDrawable(icon);
+                v.setPadding(10, 5, 10, 5);
+                linearLayout.addView(v);
+            } catch (PackageManager.NameNotFoundException e) {
+                Log.e(TAG, "Set app icon", e);
+            }
+        }
+    }
+
+    private void initPreference(Context context) {
+        mContext = context;
+        setLayoutResource(R.layout.preference_selected_apps_view);
+        mPm = context.getPackageManager();
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+}
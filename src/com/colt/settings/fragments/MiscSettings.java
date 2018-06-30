423e2e0bdc6bca7d38d762da9f33be6ae20df01f
maxwen <max.weninger@gmail.com>
2018-06-30 14:07:37 +0530

    DerpQuest: Make aspect ratio hack configable on app level [2/2]
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- src/com/colt/settings/fragments/MiscSettings.java
+++ src/com/colt/settings/fragments/MiscSettings.java
@@ -22,14 +22,28 @@ import androidx.preference.SwitchPreference;
 import android.provider.Settings;
 import com.android.settings.R;
 import com.colt.settings.utils.Utils;
+import android.text.TextUtils;
+import com.colt.settings.preference.AppMultiSelectListPreference;
+import com.colt.settings.preference.ScrollAppsViewPreference;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import com.android.settings.SettingsPreferenceFragment;
 
 public class MiscSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
+
+    private static final String KEY_ASPECT_RATIO_APPS_ENABLED = "aspect_ratio_apps_enabled";
+    private static final String KEY_ASPECT_RATIO_APPS_LIST = "aspect_ratio_apps_list";
+    private static final String KEY_ASPECT_RATIO_CATEGORY = "aspect_ratio_category";
+    private static final String KEY_ASPECT_RATIO_APPS_LIST_SCROLLER = "aspect_ratio_apps_list_scroller";
+
+    private AppMultiSelectListPreference mAspectRatioAppsSelect;
+    private ScrollAppsViewPreference mAspectRatioApps;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -39,6 +53,31 @@ public class MiscSettings extends SettingsPreferenceFragment implements
         final String KEY_DEVICE_PART_PACKAGE_NAME = "com.oneplus.shit.settings";
 
         addPreferencesFromResource(R.xml.colt_settings_misc);
+
+      final PreferenceCategory aspectRatioCategory =
+          (PreferenceCategory) getPreferenceScreen().findPreference(KEY_ASPECT_RATIO_CATEGORY);
+      final boolean supportMaxAspectRatio =
+          getResources().getBoolean(com.android.internal.R.bool.config_haveHigherAspectRatioScreen);
+      if (!supportMaxAspectRatio) {
+          getPreferenceScreen().removePreference(aspectRatioCategory);
+      } else {
+        mAspectRatioAppsSelect =
+            (AppMultiSelectListPreference) findPreference(KEY_ASPECT_RATIO_APPS_LIST);
+        mAspectRatioApps =
+            (ScrollAppsViewPreference) findPreference(KEY_ASPECT_RATIO_APPS_LIST_SCROLLER);
+        final String valuesString = Settings.System.getString(getContentResolver(),
+            Settings.System.OMNI_ASPECT_RATIO_APPS_LIST);
+        List<String> valuesList = new ArrayList<String>();
+        if (!TextUtils.isEmpty(valuesString)) {
+          valuesList.addAll(Arrays.asList(valuesString.split(":")));
+          mAspectRatioApps.setVisible(true);
+          mAspectRatioApps.setValues(valuesList);
+        } else {
+          mAspectRatioApps.setVisible(false);
+        }
+        mAspectRatioAppsSelect.setValues(valuesList);
+        mAspectRatioAppsSelect.setOnPreferenceChangeListener(this);
+      }
 
         // DeviceParts
         if (!Utils.isPackageInstalled(getActivity(), KEY_DEVICE_PART_PACKAGE_NAME)) {
@@ -57,7 +96,20 @@ public class MiscSettings extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-
+	if (preference == mAspectRatioAppsSelect) {
+        Collection<String> valueList = (Collection<String>) objValue;
+        mAspectRatioApps.setVisible(false);
+        if (valueList != null) {
+          Settings.System.putString(getContentResolver(),
+              Settings.System.OMNI_ASPECT_RATIO_APPS_LIST, TextUtils.join(":", valueList));
+          mAspectRatioApps.setVisible(true);
+          mAspectRatioApps.setValues(valueList);
+        } else {
+          Settings.System.putString(getContentResolver(),
+              Settings.System.OMNI_ASPECT_RATIO_APPS_LIST, "");
+        }
+        return true;
+      }
         return false;
     }
 
