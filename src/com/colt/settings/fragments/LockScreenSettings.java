91bedcd4c126472325218a21b50a31a33d092584
Pranav Vashi <neobuddy89@gmail.com>
2020-01-05 02:30:40 +0530

    Add toggle to hide lock icon on lockscreen [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/colt/settings/fragments/LockScreenSettings.java
+++ src/com/colt/settings/fragments/LockScreenSettings.java
@@ -82,6 +82,12 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
         return false;
     }
 
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+	Settings.System.putIntForUser(resolver,
+                Settings.System.LOCKSCREEN_LOCK_ICON, 1, UserHandle.USER_CURRENT);
+    }
+
     @Override
     public int getMetricsCategory() {
 	return MetricsProto.MetricsEvent.COLT;
