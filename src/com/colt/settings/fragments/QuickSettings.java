Evolution-X-AOSP/packages_apps_Evolver

Allow customizing footer text [2/2]

src/com/evolution/settings/fragments/QuickSettings.java
@@ -50,6 +50,7 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 
 import com.evolution.settings.preference.CustomSeekBarPreference;
+import com.evolution.settings.preference.SystemSettingEditTextPreference;
 
 import java.util.Arrays;
 import java.util.ArrayList;
@@ -63,11 +64,11 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener, Indexable {
 
     private static final String QUICK_PULLDOWN = "quick_pulldown";
-
+    private static final String FOOTER_TEXT_STRING = "footer_text_string";
     private static final String KEY_QS_PANEL_ALPHA = "qs_panel_alpha";
 
     private ListPreference mQuickPulldown;
-
+    private SystemSettingEditTextPreference mFooterString;
     private CustomSeekBarPreference mQsPanelAlpha;
 
     @Override
@@ -91,6 +92,18 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                 Settings.System.QS_PANEL_BG_ALPHA, 221);
         mQsPanelAlpha.setValue((int)(((double) qsPanelAlpha / 255) * 100));
         mQsPanelAlpha.setOnPreferenceChangeListener(this);
+
+        mFooterString = (SystemSettingEditTextPreference) findPreference(FOOTER_TEXT_STRING);
+        mFooterString.setOnPreferenceChangeListener(this);
+        String footerString = Settings.System.getString(getContentResolver(),
+                FOOTER_TEXT_STRING);
+        if (footerString != null && footerString != "")
+            mFooterString.setText(footerString);
+        else {
+            mFooterString.setText("#KeepEvolving");
+            Settings.System.putString(getActivity().getContentResolver(),
+                    Settings.System.FOOTER_TEXT_STRING, "#KeepEvolving");
+        }
     }
 
     @Override
@@ -107,6 +120,17 @@ public class QuickSettings extends SettingsPreferenceFragment implements
             int trueValue = (int) (((double) bgAlpha / 100) * 255);
             Settings.System.putInt(getContentResolver(),
                     Settings.System.QS_PANEL_BG_ALPHA, trueValue);
+            return true;
+        } else if (preference == mFooterString) {
+            String value = (String) newValue;
+            if (value != "" && value != null)
+                Settings.System.putString(getActivity().getContentResolver(),
+                        Settings.System.FOOTER_TEXT_STRING, value);
+            else {
+                mFooterString.setText("#KeepEvolving");
+                Settings.System.putString(getActivity().getContentResolver(),
+                        Settings.System.FOOTER_TEXT_STRING, "#KeepEvolving");
+            }
             return true;
         }
         return false;

Colt-Enigma/platform_packages_apps_ColtEnigma

src/com/colt/settings/fragments/QuickSettings.java
@@ -26,6 +26,7 @@ import java.util.ArrayList;
 
 import com.colt.settings.preference.CustomSeekBarPreference;
 import com.colt.settings.preference.SystemSettingSeekBarPreference;
+import com.colt.settings.preference.SystemSettingEditTextPreference;
 
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
@@ -35,12 +36,14 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private static final String KEY_QS_PANEL_ALPHA = "qs_panel_alpha";
     private static final String QS_PANEL_COLOR = "qs_panel_color";
     private static final String QS_BLUR_INTENSITY = "qs_blur_intensity";
+    private static final String X_FOOTER_TEXT_STRING = "x_footer_text_string";
 
     static final int DEFAULT_QS_PANEL_COLOR = 0xffffffff;
 
     private ColorPickerPreference mQsPanelColor;
     private SystemSettingSeekBarPreference mQsPanelAlpha;
     private CustomSeekBarPreference mQsBlurIntensity;
+    private SystemSettingEditTextPreference mFooterString;
 
     private static final String QUICK_PULLDOWN = "quick_pulldown";
 
@@ -82,6 +85,18 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         mQsBlurIntensity.setValue(qsBlurIntensity);
         mQsBlurIntensity.setOnPreferenceChangeListener(this);
 
+	mFooterString = (SystemSettingEditTextPreference) findPreference(X_FOOTER_TEXT_STRING);
+        mFooterString.setOnPreferenceChangeListener(this);
+        String footerString = Settings.System.getString(getContentResolver(),
+                X_FOOTER_TEXT_STRING);
+        if (footerString != null && footerString != "")
+            mFooterString.setText(footerString);
+        else {
+            mFooterString.setText("ColtOS");
+            Settings.System.putString(getActivity().getContentResolver(),
+                    Settings.System.X_FOOTER_TEXT_STRING, "ColtOS");
+        }
+
         }
 
     @Override
@@ -111,6 +126,17 @@ public class QuickSettings extends SettingsPreferenceFragment implements
             int valueInt = (Integer) newValue;
             Settings.System.putInt(getContentResolver(),
                     Settings.System.QS_BLUR_INTENSITY, valueInt);
+            return true;
+	} else if (preference == mFooterString) {
+            String value = (String) newValue;
+            if (value != "" && value != null)
+                Settings.System.putString(getActivity().getContentResolver(),
+                        Settings.System.X_FOOTER_TEXT_STRING, value);
+            else {
+                mFooterString.setText("MSM-Xtended");
+                Settings.System.putString(getActivity().getContentResolver(),
+                        Settings.System.X_FOOTER_TEXT_STRING, "MSM-Xtended");
+            }
             return true;
 	}
         return false;