Project-Xtended/packages_apps_Xtensions

GamingMode: Add our HW keys support [3/3]
SDB: Also remove the switch from now-hw-key devices
     and remove gesture hide toggle from HW Key Devices

src/com/xtended/fragments/GamingModeSettings.java
@@ -28,13 +28,23 @@ import androidx.preference.PreferenceScreen;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import com.android.internal.util.hwkeys.ActionUtils;
+
 import java.util.ArrayList;
 
 import com.xtended.preferences.PackageListPreference;
 
+import com.xtended.support.preferences.SystemSettingSwitchPreference;
+
 public class GamingModeSettings extends SettingsPreferenceFragment {
 
+    private static final String GAMING_MODE_DISABLE_HW_KEYS = "gaming_mode_disable_hw_keys";
+    private static final String GAMING_MODE_DISABLE_GESTURE = "gaming_mode_disable_gesture";
+
     private PackageListPreference mGamingPrefList;
+
+    private SystemSettingSwitchPreference mHardwareKeysDisable;
+    private SystemSettingSwitchPreference mGestureDisable;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -43,9 +53,19 @@ public class GamingModeSettings extends SettingsPreferenceFragment {
         addPreferencesFromResource(R.xml.x_settings_gaming);
 
         final PreferenceScreen prefScreen = getPreferenceScreen();
+        final boolean hasNavbar = ActionUtils.hasNavbarByDefault(getActivity());
 
         mGamingPrefList = (PackageListPreference) findPreference("gaming_mode_app_list");
         mGamingPrefList.setRemovedListKey(Settings.System.GAMING_MODE_REMOVED_APP_LIST);
+
+        mHardwareKeysDisable = (SystemSettingSwitchPreference) findPreference(GAMING_MODE_DISABLE_HW_KEYS);
+        mGestureDisable = (SystemSettingSwitchPreference) findPreference(GAMING_MODE_DISABLE_GESTURE);
+
+        if (hasNavbar) {
+            prefScreen.removePreference(mHardwareKeysDisable);
+        } else {
+            prefScreen.removePreference(mGestureDisable);
+        }
     }
 
     @Override

Colt-Enigma/platform_packages_apps_ColtEnigma

Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

src/com/colt/enigma/fragments/GamingModeSettings.java
@@ -28,13 +28,23 @@ import androidx.preference.PreferenceScreen;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import com.android.internal.util.hwkeys.ActionUtils;
+
 import java.util.ArrayList;
 
 import com.colt.enigma.preference.PackageListPreference;
 
+import com.colt.enigma.preference.SystemSettingSwitchPreference;
+
 public class GamingModeSettings extends SettingsPreferenceFragment {
 
+    private static final String GAMING_MODE_DISABLE_HW_KEYS = "gaming_mode_disable_hw_keys";
+    private static final String GAMING_MODE_DISABLE_GESTURE = "gaming_mode_disable_gesture";
+
     private PackageListPreference mGamingPrefList;
+
+    private SystemSettingSwitchPreference mHardwareKeysDisable;
+    private SystemSettingSwitchPreference mGestureDisable;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -43,9 +53,19 @@ public class GamingModeSettings extends SettingsPreferenceFragment {
         addPreferencesFromResource(R.xml.x_settings_gaming);
 
         final PreferenceScreen prefScreen = getPreferenceScreen();
+        final boolean hasNavbar = ActionUtils.hasNavbarByDefault(getActivity());
 
         mGamingPrefList = (PackageListPreference) findPreference("gaming_mode_app_list");
         mGamingPrefList.setRemovedListKey(Settings.System.GAMING_MODE_REMOVED_APP_LIST);
+
+        mHardwareKeysDisable = (SystemSettingSwitchPreference) findPreference(GAMING_MODE_DISABLE_HW_KEYS);
+        mGestureDisable = (SystemSettingSwitchPreference) findPreference(GAMING_MODE_DISABLE_GESTURE);
+
+        if (hasNavbar) {
+            prefScreen.removePreference(mHardwareKeysDisable);
+        } else {
+            prefScreen.removePreference(mGestureDisable);
+        }
     }
 
     @Override