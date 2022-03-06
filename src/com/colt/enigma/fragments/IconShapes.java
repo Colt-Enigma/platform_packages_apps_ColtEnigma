AospExtended/platform_packages_apps_Extensions

Bring back icon shapes, icon packs, fonts Customisation
Signed-off-by: sb6596 <shubhamprince111@gmail.com>
Change-Id: I0a80a90faa83d94ec8652fc48faee8e1ae710f09
Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

src/org/aospextended/extensions/fragments/IconShapes.java
@@ -1,0 +1,208 @@
+/*
+ * Copyright (C) 2021 AospExtended ROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.aospextended.extensions.fragments;
+
+import static com.android.internal.util.aospextended.ThemeUtils.ICON_SHAPE_KEY;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.content.res.Resources;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.os.Bundle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.text.TextUtils;
+import androidx.preference.PreferenceViewHolder;
+import android.view.ViewGroup.LayoutParams;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.graphics.ColorUtils;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
+import androidx.recyclerview.widget.RecyclerView;
+import android.net.Uri;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceScreen;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.Indexable;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settingslib.Utils;
+
+import com.bumptech.glide.Glide;
+
+import com.android.internal.util.aospextended.ThemeUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Arrays;
+
+import org.json.JSONObject;
+import org.json.JSONException;
+
+public class IconShapes extends SettingsPreferenceFragment {
+
+    private RecyclerView mRecyclerView;
+    private ThemeUtils mThemeUtils;
+
+    private String mCategory = ICON_SHAPE_KEY;
+
+    private List<String> mPkgs;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().setTitle(R.string.theme_customization_icon_shape_title);
+
+        mThemeUtils = new ThemeUtils(getActivity());
+        mPkgs = mThemeUtils.getOverlayPackagesForCategory(mCategory, "android");
+        Collections.sort(mPkgs);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(
+                R.layout.item_view, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 3);
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+        Adapter mAdapter = new Adapter(getActivity());
+        mRecyclerView.setAdapter(mAdapter);
+
+        return view;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.EXTENSIONS;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public class Adapter extends RecyclerView.Adapter<Adapter.CustomViewHolder> {
+        Context context;
+        String mSelectedPkg;
+        String mAppliedPkg;
+
+        public Adapter(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_option, parent, false);
+            CustomViewHolder vh = new CustomViewHolder(v);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(CustomViewHolder holder, final int position) {
+            String pkg = mPkgs.get(position);
+
+            holder.image.setBackgroundDrawable(mThemeUtils.createShapeDrawable(pkg));
+
+            String currentPackageName = mThemeUtils.getOverlayInfos(mCategory).stream()
+                .filter(info -> info.isEnabled())
+                .map(info -> info.packageName)
+                .findFirst()
+                .orElse("android");
+
+            holder.name.setText("android".equals(pkg) ? "Default" : getLabel(holder.name.getContext(), pkg));
+
+            final boolean isDefault = "android".equals(currentPackageName) && "android".equals(pkg);
+            final int color = ColorUtils.setAlphaComponent(
+                     Utils.getColorAttrDefaultColor(getContext(), android.R.attr.colorAccent),
+                     pkg.equals(currentPackageName) || isDefault ? 170 : 75);
+            holder.image.setBackgroundTintList(ColorStateList.valueOf(color));
+
+            holder.itemView.findViewById(R.id.option_tile).setBackgroundDrawable(null);
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    enableOverlays(position);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mPkgs.size();
+        }
+
+        public class CustomViewHolder extends RecyclerView.ViewHolder {
+            TextView name;
+            ImageView image;
+            public CustomViewHolder(View itemView) {
+                super(itemView);
+                name = (TextView) itemView.findViewById(R.id.option_label);
+                image = (ImageView) itemView.findViewById(R.id.option_thumbnail);
+            }
+        }
+    }
+
+    public Drawable getDrawable(Context context, String pkg, String drawableName) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            Resources res = pkg.equals("android") ? Resources.getSystem()
+                    : pm.getResourcesForApplication(pkg);
+            return res.getDrawable(res.getIdentifier(drawableName, "drawable", pkg));
+        }
+        catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String getLabel(Context context, String pkg) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            return pm.getApplicationInfo(pkg, 0)
+                    .loadLabel(pm).toString();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return pkg;
+    }
+
+    public void enableOverlays(int position) {
+        mThemeUtils.setOverlayEnabled(mCategory, mPkgs.get(position));
+    }
+}

Colt-Enigma/platform_packages_apps_ColtEnigma

Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

src/com/colt/enigma/fragments/IconShapes.java
@@ -1,0 +1,208 @@
+/*
+ * Copyright (C) 2021 AospExtended ROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.colt.enigma.fragments;
+
+import static com.android.internal.util.colt.ThemeUtils.ICON_SHAPE_KEY;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.content.res.Resources;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.os.Bundle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.text.TextUtils;
+import androidx.preference.PreferenceViewHolder;
+import android.view.ViewGroup.LayoutParams;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.graphics.ColorUtils;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
+import androidx.recyclerview.widget.RecyclerView;
+import android.net.Uri;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceScreen;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.Indexable;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settingslib.Utils;
+
+import com.bumptech.glide.Glide;
+
+import com.android.internal.util.colt.ThemeUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Arrays;
+
+import org.json.JSONObject;
+import org.json.JSONException;
+
+public class IconShapes extends SettingsPreferenceFragment {
+
+    private RecyclerView mRecyclerView;
+    private ThemeUtils mThemeUtils;
+
+    private String mCategory = ICON_SHAPE_KEY;
+
+    private List<String> mPkgs;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().setTitle(R.string.theme_customization_icon_shape_title);
+
+        mThemeUtils = new ThemeUtils(getActivity());
+        mPkgs = mThemeUtils.getOverlayPackagesForCategory(mCategory, "android");
+        Collections.sort(mPkgs);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(
+                R.layout.item_view, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 3);
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+        Adapter mAdapter = new Adapter(getActivity());
+        mRecyclerView.setAdapter(mAdapter);
+
+        return view;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.COLT;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public class Adapter extends RecyclerView.Adapter<Adapter.CustomViewHolder> {
+        Context context;
+        String mSelectedPkg;
+        String mAppliedPkg;
+
+        public Adapter(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_option, parent, false);
+            CustomViewHolder vh = new CustomViewHolder(v);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(CustomViewHolder holder, final int position) {
+            String pkg = mPkgs.get(position);
+
+            holder.image.setBackgroundDrawable(mThemeUtils.createShapeDrawable(pkg));
+
+            String currentPackageName = mThemeUtils.getOverlayInfos(mCategory).stream()
+                .filter(info -> info.isEnabled())
+                .map(info -> info.packageName)
+                .findFirst()
+                .orElse("android");
+
+            holder.name.setText("android".equals(pkg) ? "Default" : getLabel(holder.name.getContext(), pkg));
+
+            final boolean isDefault = "android".equals(currentPackageName) && "android".equals(pkg);
+            final int color = ColorUtils.setAlphaComponent(
+                     Utils.getColorAttrDefaultColor(getContext(), android.R.attr.colorAccent),
+                     pkg.equals(currentPackageName) || isDefault ? 170 : 75);
+            holder.image.setBackgroundTintList(ColorStateList.valueOf(color));
+
+            holder.itemView.findViewById(R.id.option_tile).setBackgroundDrawable(null);
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    enableOverlays(position);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mPkgs.size();
+        }
+
+        public class CustomViewHolder extends RecyclerView.ViewHolder {
+            TextView name;
+            ImageView image;
+            public CustomViewHolder(View itemView) {
+                super(itemView);
+                name = (TextView) itemView.findViewById(R.id.option_label);
+                image = (ImageView) itemView.findViewById(R.id.option_thumbnail);
+            }
+        }
+    }
+
+    public Drawable getDrawable(Context context, String pkg, String drawableName) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            Resources res = pkg.equals("android") ? Resources.getSystem()
+                    : pm.getResourcesForApplication(pkg);
+            return res.getDrawable(res.getIdentifier(drawableName, "drawable", pkg));
+        }
+        catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String getLabel(Context context, String pkg) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            return pm.getApplicationInfo(pkg, 0)
+                    .loadLabel(pm).toString();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return pkg;
+    }
+
+    public void enableOverlays(int position) {
+        mThemeUtils.setOverlayEnabled(mCategory, mPkgs.get(position));
+    }
+}