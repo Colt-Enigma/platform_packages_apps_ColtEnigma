a21214c58388f06194efb317b5fdb066f01dfb31
RakeshBatra <raakesh.batra@rediffmail.com>
2023-02-12 10:57:38 +0000

    misc corrections
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- src/com/colt/enigma/fragments/QsTileLayoutSettings.java
+++ src/com/colt/enigma/fragments/QsTileLayoutSettings.java
@@ -25,7 +25,7 @@ import android.widget.Toast;
 import androidx.preference.Preference;
 
 import com.android.internal.logging.nano.MetricsProto;
-import com.android.internal.util.banana.BananaUtils.QSLayoutUtils;
+import com.android.internal.util.colt.ColtUtils.QSLayoutUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
